c4c7cd5d6c187ff8d59c3bf6e79d5dd272c02e23
IvannaTokarska <tokarskaya.ivanna@gmail.com>
2024-07-29 22:25:08 +0300

    modified: js/form.js

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@ const form = document.querySelector('#form');
 const launchBtn = document.querySelector('#launch-btn');
 const goToFormButton = document.querySelector('#go-to-form-btn');
 const userEmailField = document.querySelector('#user-email');
+const userNameField = document.querySelector('#user-name');
+
 
 
 goToFormButton.addEventListener('click', function (e) {
@@ -10,51 +12,51 @@ goToFormButton.addEventListener('click', function (e) {
 });
 
 function clearFormFields() {
-    const modalFiends = formModal.querySelectorAll('input');
+    const fieldName = form.querySelector('input[type="text"]');
+    const fieldEmail = form.querySelector('input[type="email"]');
 
-    modalFiends.forEach( field => { 
-        field.value = ''
-    });
+    fieldName.value = '';
+    fieldEmail.value = '';
 }
 
+function addGooseElement() {
+    const targetContainer = document.querySelector('#form');
+    const gooseEl = document.createElement('img');
+    gooseEl.classList.add('gus-anim');
+
+    targetContainer.appendChild(gooseEl);
+}
 
 function showGooseAnim() {
-    const targetContainer = document.querySelector('#form');
-    const gusImage = document.createElement('img');
-    gusImage.setAttribute('src', './img/gus-anim.gif');
-    gusImage.classList.add('gus-anim');
+    const gooseEl = document.querySelector('.gus-anim');
 
-    targetContainer.appendChild(gusImage);
+    gooseEl.setAttribute('src', './img/gus-anim.gif');
+    
 
-    setTimeout(2000, () => {
-        targetContainer.removeChild(gusImage);
-    })
+    setTimeout(() => {
+        gooseEl.removeAttribute('src');
+    }, 4000)
 }
 
+addGooseElement();
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    launchBtn.setAttribute('disabled', true)
-
     if (userEmailField?.value?.length > 30) {
         return;
     }
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => {
-        showGooseAnim();
+    console.log('Імʼя користувача: ', userNameField.value);
+    console.log('Email користувача: ', userEmailField.value);
 
-        setTimeout(() => {
-            // launchBtn.removeAttribute('disabled')
-            // clearFormFields();
-        }, 2000);
-      })
-      .catch((error) => console.log('Sending form failed'));
+    launchBtn.setAttribute('disabled', true);
+    launchBtn.style.opacity = '0.7';
+
+    showGooseAnim();
+
+    setTimeout(() => {
+        launchBtn.style.opacity = '1';
+    }, 4000)
 })
-
